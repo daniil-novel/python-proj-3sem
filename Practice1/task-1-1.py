97ef78e968ac24ed5614a7d19845e2d0c96d179b
Daniil <daniilpriwesentsew@gmail.com>
2023-02-13 00:43:40 +0300

    Task1 implemented

--- Practice1/task-1-1.py
+++ Practice1/task-1-1.py
@@ -1,1 +1,12 @@
-print("Hello World")
+int = 42
+string = 42
+float = 42.0
+char = "42"
+binary = 0b00101010
+hexadecimal = 0x2A
+octal = 0o52
+charA = '42'
+tuple = (42)
+lst = ["42"," "]
+
+print(int, string, float, char, binary, hexadecimal, octal, charA, tuple, lst[0])
