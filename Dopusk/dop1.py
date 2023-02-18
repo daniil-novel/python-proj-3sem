26c2ce95ae6b41ccbe8dec105b403c7986a9395e
Daniil <daniilpriwesentsew@gmail.com>
2023-02-18 19:18:56 +0300

    dop1 complete

--- Dopusk/dop1.py
+++ Dopusk/dop1.py
@@ -2,12 +2,18 @@ import math
 
 
 def main(x, z):
-    return (math.log2(z**3 + 1) ** 3 - (math.asin(1 + x + x ** 2) ** 5) / (49 * (z ** 3) - 63 * (x ** 5)) - (z ** 6 - (x + 60 * (x ** 3) + 20 * (z ** 2)) ** 5) / (x ** 5 + x ** 7)
+    F = (math.log2(z ** 3 + 1) ** 3 - (math.asin(1 + x + x ** 2) ** 5)) / (49 * (z ** 3) - 63 * (x ** 5)) - (z ** 6 - (x + 60 * (x ** 3) + 20 * (z ** 2)) ** 5) / (x ** 5 + z ** 7)
+    return F
 
+print('{:.2e}'.format(main(-0.24, -0.45)))
+print('{:.2e}'.format(main(-0.7, 0.17)))
+print('{:.2e}'.format(main(-0.2, 0.42)))
+print('{:.2e}'.format(main(-0.19, -0.11)))
+print('{:.2e}'.format(main(-0.76, 0.83)))
 
-print(main(-0.24, -0.45))
 '''
-print(main(-0.7, 0.17), "/n")
+print(main(-0.24, -0.45))
+
 print(main(-0.2, 0.42), "/n")
 print(main(-0.19, -0.11), "/n")
 print(main(-0.76, 0.83), "/n")
